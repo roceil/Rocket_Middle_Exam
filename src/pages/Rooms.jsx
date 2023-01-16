93667004cac982591584eeda2dcf055af21293e2
ChiaYang <aa235553756@gmail.com>
2023-01-16 11:00:44 +0800

    feature/fix Rooms Layout

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -35,11 +35,11 @@ export function Rooms() {
       <nav className="w-[42%] h-full fixed">
         {/* 輪播圖 */}
         <RoomCarousel />
-        <div className="">
+        <div className="buttonGroup flex relative h-screen pt-[87px] pb-[25vh] flex-col justify-between z-10">
           {/* 返回首頁按鈕 */}
           <button
             type="button"
-            className="flex items-center absolute top-[87px] left-[128px]"
+            className="flex items-center pl-[13vh]"
           >
             <img src={backHome} alt="backHome" className="m-[10px] " />
             <NavLink to="/" className="font-light text-sm 2xl:text-base 3xl:text-lg text-primary">
@@ -47,7 +47,7 @@ export function Rooms() {
             </NavLink >
           </button>
           {/* 價格＆預約按鈕 */}
-          <div className=" flex flex-col items-center absolute bottom-[109px] left-[161px]">
+          <div className=" flex flex-col items-center">
             <div className="mb-[10px]">
               <span className="text-[36px] text-primary">$1,380 </span>
               <span className="text-xl text-primary"> / 1晚</span>
@@ -63,31 +63,33 @@ export function Rooms() {
           </div>
         </div>
       </nav>
-      {/* 因nav改fixed出現的佔位格 */}
-      <div className="w-[42%] mr-[30px] flex-shrink-0 "></div>
-      {/* 房間細節 */}
-      <div className="h-[200vh] mt-[13vh] mr-[13vh] w-[635px] text-primary">
-        <RoomDetail />
-        {/* icons */}
-        <ul className="flex flex-wrap gap-x-10 gap-y-[26px] mb-7">
-          {/* 01 */}
-          {iconAry.map((item, i) => {
-            return (
-              <li key={i} className="flex">
-                <img src={AC} alt="" />
-                <img
-                  src={OK}
-                  alt=""
-                  className="relative -top-[13px] -right-1 "
-                />
-              </li>
-            );
-          })}
-        </ul>
-        <p className="text-primary text-sm font-medium mb-2 leading-6">空房狀態查詢</p>
-        {/* 日曆佔位格 */}
-        <div className="h-[50vh] bg-red-400">
-          <h1>我大概率是日曆</h1>
+      <div className="container flex">
+        {/* 因nav改fixed出現的佔位格 */}
+        <div className="w-[42%] mr-[30px] flex-shrink-0 "></div>
+        {/* 房間細節 */}
+        <div className="h-[200vh] mt-[13vh]  w-[635px] text-primary">
+          <RoomDetail />
+          {/* icons */}
+          <ul className="flex flex-wrap gap-x-10 gap-y-[26px] mb-7">
+            {/* 01 */}
+            {iconAry.map((item, i) => {
+              return (
+                <li key={i} className="flex">
+                  <img src={AC} alt="" />
+                  <img
+                    src={OK}
+                    alt=""
+                    className="relative -top-[13px] -right-1 "
+                  />
+                </li>
+              );
+            })}
+          </ul>
+          <p className="text-primary text-sm font-medium mb-2 leading-6">空房狀態查詢</p>
+          {/* 日曆佔位格 */}
+          <div className="h-[50vh] bg-red-400">
+            <h1>我大概率是日曆</h1>
+          </div>
         </div>
       </div>
 
