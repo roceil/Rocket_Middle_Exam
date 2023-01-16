6c877550f629d36480c58c76af0f90a6c02105a2
Sophie Lee <2020soop@gmail.com>
2023-01-16 12:39:41 +0800

    feature/Room info list

--- src/container/RoomDetail.jsx
+++ src/container/RoomDetail.jsx
@@ -6,16 +6,28 @@ function RoomDetail() {
   const token = 'Bearer IAlFGuHujADexllpJHWL1MenPYbizgbL00yxoV8wLs9zfZxS4hgs0wVo6E6b';
   const authorization = { 'headers': { 'Authorization': token } };
   const id = '3Elqe8kfMxdZv5xFLV4OUeN6jhmxIvQSTyj4eTgIowfIRvF4rerA2Nuegzc2Rgwu';
+  // 抓 data
   const [roomData, setRoomData] = useState({});
+  // 房型資訊斷句
+  const [roomInfo, setRoomInfo] = useState([]);
 
   console.log(1, '渲染中');
+
   useEffect(() => {
     console.log('2,渲染完後得effect')
     const getRoomInfo = async () => {
       const res = await axios.get(`${url}/${id}`, authorization)
       console.log(res.data.room[0]);
       setRoomData(res.data.room[0]);
+      // 處理斷句
+      setRoomInfo(res.data.room[0].description.split(/\.\s+/))
+      // 字串轉中文（待處理）
+      // roomDescription = new Intl.Locale(roomData.description, 'zh-TW');
+      //   if(roomData.description !== undefined){
+      //   roomDescription = new Intl.Locale(roomData.description, 'zh-TW');
+      // }
     }
+
     getRoomInfo();
   }, [])
   console.log(roomData);
@@ -31,11 +43,9 @@ function RoomDetail() {
         退房時間：{roomData.checkInAndOut?.checkOut}
       </p>
       <ul className="text-primary text-sm font-light mb-12 leading-6">
-        <li>{roomData.description}</li>
-        <li>・單人間僅供一位客人使用。</li>
-        <li>・臥室配有單人床和私人浴室。</li>
-        <li>・您需要的一切為您準備：床單和毯子，毛巾，肥皂和洗髮水，吹風機。</li>
-        <li>・房間裡有AC，當然還有WiFi。</li>
+        {roomInfo.map((item,index) => (
+            <li key={index}>・{item}</li>
+        ))}
       </ul>
     </>
   )
